1020e0359a74a3daeffbe6b89799b23ed1b52cf8
Sara Szwejkowska <saraszw@op.pl>
2022-12-15 21:28:25 +0100

    created function math random and functions that are looking for a text and url

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 const photos = document.querySelector('.flex');
-const firstPhoto = document.querySelector('.gallery img1');
-const secondPhoto = document.querySelector('.gallery img2');
-const thirdPhoto = document.querySelector('.gallery img3');
-const fourPhoto = document.querySelector('.gallery img4');
-const fivePhoto = document.querySelector('.gallery img5');
-const sixPhoto = document.querySelector('.gallery img6');
-const sevenPhoto = document.querySelector('.gallery img7');
-const eightPhoto = document.querySelector('.gallery img8');
-const ninePhoto = document.querySelector('.gallery img9');
-const tenPhoto = document.querySelector('.gallery img10');
-const elevenPhoto = document.querySelector('.gallery img11');
-const twelvePhoto = document.querySelector('.gallery img12');
+const allPhotos = document.querySelectorAll('.gallery.img');
+const allTitles = document.querySelectorAll('.title')
 
 async function getData() {
-	const response = await fetch('https://picsum.photos/200');
+	const randomNumbers = Math.floor(Math.random() * 20 + 1);
+	const response = await fetch(
+		`https://picsum.photos/v2/list?page=${randomNumbers}&limit=12`
+	);
 	const data = await response.json();
 	return data;
 }
-getData().then((foto) => {
-	foto.forEach((elem) => {
-		console.log(foto.img);
+
+getData().then((data) => {
+	allTitles.forEach((element, index) => {
+		element.innerHTML = data[index].author
+	})
+	allPhotos.forEach((element, index) => {
+		element.style.backgroundImage = `url(${data[index].download_url})`;
 	});
-	window.onload = function () {
-		let photo = document.createElement('img');
-		document.getElementsByClassName('gallery img1').appendChild(photo);
-	};
 });
 
-
+// getData().then((foto) => {
+// 	foto.forEach((elem) => {
+// 		console.log(elem.img);
+// 	});
+// 	// window.onload = function () {
+// 	// 	let photo = document.createElement('img');
+// 	// 	document.getElementsByClassName('gallery img').appendChild(photo);
+// 	// };
+// });
+// img.addEventListener('click', () => {
+// 	author - hidden.classList.remove('hidden');
+// });
